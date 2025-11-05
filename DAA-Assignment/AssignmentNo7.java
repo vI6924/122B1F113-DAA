33e25e81136d5f63548d31cd02f600b86a957402
vI6924 <viveksalunke694@gmail.com>
2025-11-05 12:48:25 +0530

    Modify comment with course code
    Updated comment to include course code.

--- DAA-Assignment/AssignmentNo7.java
+++ DAA-Assignment/AssignmentNo7.java
@@ -1,5 +1,5 @@
 // Universitytime table scheduling ,implement graph coloring technique to genrate conflict force and efficient time table
-122B1F113
+//122B1F113
 import java.util.*;
 
 public class AssignmentNo7 {
